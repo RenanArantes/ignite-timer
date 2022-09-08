f3f8cde077a6475035c830b09822b8bf1404e1e2
RenanArantes <malevo18@gmail.com>
2022-09-08 14:51:49 -0300

    Mudando o title da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -49,12 +49,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      console.log('ativou o useEffect')
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -70,11 +77,12 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(createdCycleId)
+    setAmountSecondsPassed(0)
 
     reset()
   }
 
-  console.log(formState.errors)
+  // console.log(formState.errors)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -84,6 +92,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0') // preenche uma string com algum caractere específico
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes} : ${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task

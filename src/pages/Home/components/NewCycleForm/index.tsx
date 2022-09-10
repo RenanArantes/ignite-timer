5e2bbe4bfc2e54935e98f81cc6de999708512511
RenanArantes <malevo18@gmail.com>
2022-09-10 14:53:17 -0300

    !!!Prop Drilling no React - código não funcional

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,35 +1,58 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { FormContainer, TaskInput, MinutesAmountInput } from './styles'
 
-export const NewCycleForm() => {
+import { useForm } from 'react-hook-form'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa.'),
+  minutesAmount: zod
+    .number()
+    .min(1)
+    .max(60, 'O ciclo precisa ser no máximo de 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function NewCycleForm() {
+  const { register, handleSubmit, watch, formState, reset } =
+    useForm<NewCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: '',
+        minutesAmount: 0,
+      },
+    })
+
   return (
     <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            id="task"
-            list="task-suggestions"
-            placeholder="Dê um nome para seu projeto."
-            disabled={!!activeCycle}
-            {...register('task')}
-          />
-          <label htmlFor="minutesAmount">durante</label>
-          <datalist id="task-suggestions">
-            <option value="valor1" />
-            <option value="valor2" />
-            <option value="valor3" />
-            <option value="valor4" />
-          </datalist>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para seu projeto."
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+      <label htmlFor="minutesAmount">durante</label>
+      <datalist id="task-suggestions">
+        <option value="valor1" />
+        <option value="valor2" />
+        <option value="valor3" />
+        <option value="valor4" />
+      </datalist>
 
-          <MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            placeholder="00"
-            step={5}
-            min={1}
-            max={60}
-            disabled={!!activeCycle}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
-          <span>minutos.</span>
-        </FormContainer>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
   )
 }

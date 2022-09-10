6883c12b9ec809eefcc768f7e241df9e3745fcdf
RenanArantes <malevo18@gmail.com>
2022-09-10 15:34:33 -0300

    Entendendo Estados

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Home } from './Home'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
+    // <ThemeProvider theme={defaultTheme}>
+    //   <BrowserRouter>
+    //     <Router />
+    //   </BrowserRouter>
+    //   <GlobalStyle />
+    // </ThemeProvider>
+    <Home />
   )
 }
---
+++ src/Home.tsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState } from 'react'
+
+const CyclesContext = createContext({} as any)
+
+function NewCycleForm() {
+  let { activeCycle, setActiveCycle } = useContext(CyclesContext)
+
+  return (
+    <div>
+      <h1>NewCycleForm:{activeCycle}</h1>
+      <button onClick={() => setActiveCycle((activeCycle += 1))}>
+        Aumentar
+      </button>
+    </div>
+  )
+}
+
+function Countdown() {
+  const { activeCycle } = useContext(CyclesContext)
+
+  return <h1>Countdown:{activeCycle}</h1>
+}
+
+export function Home() {
+  const [activeCycle, setActiveCycle] = useState(0)
+
+  return (
+    <CyclesContext.Provider value={{ activeCycle, setActiveCycle }}>
+      <div>
+        <NewCycleForm />
+        <Countdown />
+      </div>
+    </CyclesContext.Provider>
+  )
+}
